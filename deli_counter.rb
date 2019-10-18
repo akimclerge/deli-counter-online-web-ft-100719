f4350be18f9a709dacfef3313ebae9ec621381c4
Akim Clerge <akimclerge@gmail.com>
2019-10-18 00:25:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,17 +17,17 @@ end
 
 line(katz_deli)
 
-def line_simple(array) 
+def line_simple(array) # this one follows the same mechanics as learn.co
   current_line = "The simple line is currently:"
   array.each.with_index(1) do |value, indexemus|  
+  # "each.with_index" is the method...must use "index"
     current_line << " #{indexemus}. #{value},"    
+    
   end 
   puts current_line
 end 
   
-
-
-
+line_simple(katz_deli)  
 
 def take_a_number(line, new_person)
   line.push(new_person) 
@@ -40,7 +40,8 @@ def now_serving(line)
   if line.length == 0 
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." 
+    puts "Currently serving #{line[0]}." #
+    line.shift 
   end
 end
 
